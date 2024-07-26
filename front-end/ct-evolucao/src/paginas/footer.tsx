5a213f243fab3ee55993ff76d2d27d7c26d724c7
Mário Henrique <mariohenriquelp@gmail.com>
2024-07-25 23:23:02 -0300

    Adicionado BE e db

--- front-end/ct-evolucao/src/paginas/footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-    return (
-        <footer className="bg-slate-800 h-auto w-full">
-            <div>
-            <p>Desenvolvido por Mário Henrique</p>
-            </div>
-        </footer>
-    )
-}
--- front-end/ct-evolucao/src/paginas/topo1.tsx
+++
@@ -1,7 +0,0 @@
-export function Topo1() {
-    <nav>
-        <a href="#">Home</a>
-        <a href="#">Modalidades</a>
-        <a href="#">Professores</a>
-    </nav>
-}
